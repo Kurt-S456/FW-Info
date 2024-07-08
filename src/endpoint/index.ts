95d79b6615c7a68d14885cdaeabd8bfe53903cd4
KurtS <kurt.florian.schrammel@gmail.com>
2024-07-08 22:54:14 +0200

    feat: init cron task

--- src/endpoint/index.ts
+++ src/endpoint/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import { initializeScraper } from '../service/scraper';
 import * as dbQueries from '../db/queries';
+import * as cron from 'node-cron';
 
 const app: Express = express();
 const PORT: string | number = process.env.PORT || 4000;
@@ -20,4 +21,9 @@ app.get('/scrape', async (req: express.Request, res: express.Response) => {
 app.get('/articles', async (req: express.Request, res: express.Response) => {
     console.log("GET /articles");
     res.send(await dbQueries.getArticles());
+});
+
+cron.schedule('* * * * *', () => {
+    console.log("Cron job started");
+    console.log("Cron job finished");
 });
