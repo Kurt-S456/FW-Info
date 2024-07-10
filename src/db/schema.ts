113531640f45465ef459a7f19bc6949e9d29d23e
KurtS <kurt.florian.schrammel@gmail.com>
2024-07-10 14:41:10 +0200

    refactor: model

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,56 +1,43 @@
 import { sql } from 'drizzle-orm';
-import { integer, sqliteTable, text, foreignKey } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const article = sqliteTable("article", {
-  id: integer("id"),
-  title: text("title"),
-  summary: text("summary"),
+export const articleTable = sqliteTable("article", {
+  id: integer("id").primaryKey(),
+  title: text("title").notNull(),
+  summary: text("summary").notNull(),
   url: text("url"),
   imageUrl: text("image_url"),
+  departmentId: integer("department_id").notNull().references(() => departmentTable.id),
   createdAt: text('created_at')
   .default(sql`(CURRENT_TIMESTAMP)`)
   .notNull(),
 });
 
-export const department = sqliteTable("department", {
-  id: integer("id"),
+export const departmentTable = sqliteTable("department", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  url: text("url").notNull(),
+  sectionId: integer("section_id").notNull().references(() => sectionTable.id),
+});
+
+export const sectionTable = sqliteTable("section", {
+  id: integer("id").primaryKey(),
   name: text("name"),
   url: text("url"),
+  districtId: integer("district_id").notNull().references(() => distirctTable.id),
 });
 
-export const section = sqliteTable("section", {
-  id: integer("id"),
-  name: text("name"),
-  url: text("url"),
-  departmentId: integer("department_id")
-    .references(() => department.id), // Foreign key to the department table
+export const distirctTable = sqliteTable("district", {
+  id: integer("id").primaryKey(),
+  name: text("name").notNull(),
+  url: text("url").notNull(),
 });
 
-export const district = sqliteTable("district", {
-  id: integer("id"),
-  name: text("name"),
-  url: text("url"),
-  sectionId: integer("section_id")
-    .references(() => section.id), // Foreign key to the section table
-});
+export type InsertArticle = typeof articleTable.$inferInsert;
+export type SelectArticle = typeof articleTable.$inferSelect;
 
-export const departmentDistrict = sqliteTable("department_district", {
-  departmentId: integer("department_id")
-    .references(() => department.id), // Foreign key to the department table
-  districtId: integer("district_id")
-    .references(() => district.id), // Foreign key to the district table
-});
+export type SelectDepartment = typeof departmentTable.$inferSelect;
 
-export const sectionDistrict = sqliteTable("section_district", {
-  sectionId: integer("section_id")
-    .references(() => section.id), // Foreign key to the section table
-  districtId: integer("district_id")
-    .references(() => district.id), // Foreign key to the district table
-});
+export type SelectSection = typeof sectionTable.$inferSelect;
 
-export const articleDepartment = sqliteTable("article_department", {
-  articleId: integer("article_id")
-    .references(() => article.id), // Foreign key to the article table
-  departmentId: integer("department_id")
-    .references(() => department.id), // Foreign key to the department table
-});
+export type SelectDistrict = typeof distirctTable.$inferSelect;
