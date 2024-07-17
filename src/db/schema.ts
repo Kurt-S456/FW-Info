f4aa6fb54b97ea822378050ac9f461541e552664
KurtS <kurt.florian.schrammel@gmail.com>
2024-07-17 22:06:30 +0200

    feat: added paragraphs

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,6 +4,7 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 export const districtTable = sqliteTable("district", {
   id: integer("id").primaryKey(),
   name: text("name").notNull(),
+  short: text("short").notNull(),
   url: text("url").notNull(),
 });
 
@@ -25,12 +26,25 @@ export const articleTable = sqliteTable("article", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   title: text("title").notNull(),
   summary: text("summary").notNull(),
+  content: text("content"),
   url: text("url"),
   imageUrl: text("image_url"),
   departmentId: integer("department_id").notNull().references(() => departmentTable.id),
   createdAt: text('created_at')
     .default(sql`(CURRENT_TIMESTAMP)`)
     .notNull(),
+});
+
+export const tagTable = sqliteTable("tag", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  short: text("short").notNull(),
+  color: text("color"),
+});
+
+export const articleTagTable = sqliteTable("article_tag", {
+  articleId: integer("article_id").notNull().references(() => articleTable.id),
+  tagId: integer("tag_id").notNull().references(() => tagTable.id)
 });
 
 export type InsertArticle = typeof articleTable.$inferInsert;
@@ -41,7 +55,3 @@ export type SelectDepartment = typeof departmentTable.$inferSelect;
 export type SelectSection = typeof sectionTable.$inferSelect;
 
 export type SelectDistrict = typeof districtTable.$inferSelect;
-
-export function generateId(): number {
-  return Math.floor(Math.random() * 1000000);
-}
