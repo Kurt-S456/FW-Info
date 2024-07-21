0b861a788ddcb82ef7fb2e9e37a6cc5efb1b5504
KurtS <kurt.florian.schrammel@gmail.com>
2024-07-21 11:53:28 +0200

    fix: merged

--- src/endpoint/index.ts
+++ src/endpoint/index.ts
@@ -25,9 +25,21 @@ app.get('/articles', async (req: express.Request, res: express.Response) => {
 cron.schedule('* * * * *', async () => {
     console.log("Cron job started");
     const browser = await scraper.initBrowser();
+    const afkott = await afkScraper.scrapeArticlesOttenschlag(browser, 2);
+    console.log(afkott);
+    const afkgg = await afkScraper.scrapeArticlesGrossGerungs(browser, 1);
+    console.log(afkgg);
     const bfkGd = await bfkScraper.scrapeArticlesGmuend(browser, 1);
     console.log("bfkGd", bfkGd);
-    await dbQueries.createArticles(bfkGd);
+    try {
+        await dbQueries.createArticles(afkott);
+        await dbQueries.createArticles(afkgg);
+        await dbQueries.createArticles(bfkGd);
+    } catch (error) {
+        console.error(error);
+    }
+  
+    
     await browser.close();
     console.log("Cron job finished");
 });
